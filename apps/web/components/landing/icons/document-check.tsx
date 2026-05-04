9a35f844de346f225b92baf1915291a2dd30b171
TechHypeXP <104952356+TechHypeXP@users.noreply.github.com>
2026-05-04 12:38:42 +0300

    fix: add checkmark to document icon for 'Not a Diagnosis' trust card

--- apps/web/components/landing/icons/document-check.tsx
+++ apps/web/components/landing/icons/document-check.tsx
@@ -15,9 +15,7 @@ export function DocumentCheckIcon(props: React.SVGProps<SVGSVGElement>) {
     >
       <path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z" />
       <polyline points="14,2 14,8 20,8" />
-      <polyline points="10,12 16,12" />
-      <polyline points="10,16 16,16" />
-      <polyline points="10,20 14,20" />
+      <path d="M10 12l2 2 4-4" />
     </svg>
   )
 }
